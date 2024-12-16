ec98f1d7b1a4fd204dd6ce69f10e3104021f444b
Fabio <fabiodn.798@gmail.com>
2024-12-16 01:17:55 +0100

    fix: Update vite.config for app-admin

--- apps/app-admin/vite.config.ts
+++ apps/app-admin/vite.config.ts
@@ -3,25 +3,29 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../node_modules/.vite/app-admin',
   server: {
-    port: 4300,
+    port: 4500,
     host: 'localhost',
   },
   preview: {
-    port: 4300,
+    port: 4400,
     host: 'localhost',
   },
   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+
+  resolve: {
+      alias: {
+        '@shared-ui': path.resolve(__dirname, '../../libs/shared-ui/src'),
+      },
+    },
+    
   build: {
-    outDir: '../dist/app-admin',
+    outDir: '../../dist/apps/app-admin',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
