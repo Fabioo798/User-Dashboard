13d6cd28b7a592bb59e3a84d5d83236d4923d32b
Fabio <fabiodn.798@gmail.com>
2024-12-20 22:08:42 +0100

    fix: Update backend to be deployed with docker image and Render

--- apps/api/src/db/seeds/populate_users.ts
+++ apps/api/src/db/seeds/populate_users.ts
@@ -1,8 +1,13 @@
 import type { Knex } from "knex";
+import { Auth } from "../../shared/utils/auth";
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex("users").del();
+
+  const hashedPassword1 = await Auth.hash("password1");
+  const hashedPassword2 = await Auth.hash("password2");
+  const hashedPassword3 = await Auth.hash("password3");
 
   // Inserts seed entries
   await knex("users").insert([
@@ -10,7 +15,7 @@ export async function seed(knex: Knex): Promise<void> {
       id: 1,
       name: "Admin User",
       email: "admin@example.com",
-      password: "hashedpassword1", // Use bcrypt-hashed passwords
+      password: hashedPassword1, // Use bcrypt-hashed passwords
       role: "admin",
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
@@ -19,7 +24,7 @@ export async function seed(knex: Knex): Promise<void> {
       id: 2,
       name: "User One",
       email: "user1@example.com",
-      password: "hashedpassword2",
+      password: hashedPassword2,
       role: "user",
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
@@ -28,7 +33,7 @@ export async function seed(knex: Knex): Promise<void> {
       id: 3,
       name: "User Two",
       email: "user2@example.com",
-      password: "hashedpassword3",
+      password: hashedPassword3,
       role: "user",
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
